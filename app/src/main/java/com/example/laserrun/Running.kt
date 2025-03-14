7c8ce0757044d3f38bc5eccef140dd3a0f7c413a
alexisetu <alexis.gilbert01@etu.univ-poitiers.fr>
2025-03-14 21:18:26 +0100

    changement du texte du bouton de fin de course

--- app/src/main/java/com/example/laserrun/Running.kt
+++ app/src/main/java/com/example/laserrun/Running.kt
@@ -5,7 +5,13 @@ import android.graphics.Color
 
 class Running : RunningAndShooting() {
     override val destination: Class<*> = Shooting::class.java
-    override val buttonText: String by lazy { getString(R.string.arrivee_stand) }
+    override val buttonText: String by lazy { 
+        if (isLastLap) {
+            getString(R.string.fin_course)
+        } else {
+            getString(R.string.arrivee_stand)
+        }
+    }
     override val fondColor: Int by lazy { getColor(R.color.fondvert) }
     override val isLastLap: Boolean by lazy {
         val currentLap = intent.getIntExtra("CURRENT_LAP", 0)
